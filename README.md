3a58e3efc1ae9758f26b2fccc3fa787a9d707e49
Down00 <atiqahaina0612@gmail.com>
2026-05-19 13:47:09 +0800

    Update main.py and README.md version 1.1.0
    - Update menu and selected model
    - Create a README.md

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # NL2SQL
+A backend Command Line Interface (CLI) framework designed to evaluate and test various NL2SQL models.
+
+**Note:** The frontend interface for this application is currently in progress and not yet available. All interactions are handled via the CLI.
+
+## Prerequisites & Installation
+
+To run this CLI tool locally, follow these steps to set up your environment:
+
+1. Activate the Virtual Environment.
+
+    - **Windows**: venv\Scripts\Activate
+    - **macOS/Linux**: source venv/bin/activate
+
+2. Install Requirements
+    - Ensure a *requirements.txt* file exists in your project backend folder.
+
+    >> pip install -r requirements.txt
+
+3. Configure Environment Variable (.env)
+
+    **Note: Users must generate your own free access token from yiur https://huggingface.co/settings/tokens to avoid rate limits.**
+    
+    1. Create a *.env* file in the *backend/* directory
+
+    2. Add your Hugging Face *Read* Token to the file.
+
+        # Example .env file
+        HF_TOKEN=your_hugging_face_read_token_here
+
+## Usage Guide
+
+Once your environment is set up and your token is configured, you can run the CLI application.
+
+1. Navigate to the Backend Directory
+
+    >> cd backend
+
+2. Launch the Application
+
+    >> python -m app.main
+
+3. Interacting with the CLI Menu Upon running the command, you will be presented with a main menu. Choose the number corresponding to your desired action:
+
+    1. Running Question to SQL Test: Evaluates how well a model translates natural language queries into executable SQL commands.
+
+    2. Running Question Answering Test: Evaluates the end-to-end process (Question -> SQL -> Database Execution -> Natural Language Answer).
+
+    3. Exit/Quit: Closes the application.
+
+4. Model Selection & Batch Testing
+
+After selecting either option 1 or 2, the CLI will display a list of available NL2SQL models.
+
+Enter the number/name of the model you wish to test.
+
+Automatic Execution: Once a model is selected, the system will automatically begin running the batch test against the scenarios defined in scripts/test_cases.json. Sit back and wait for the reports to generate in your root folder!
+
+## 🚧 Roadmap
+- Development and integration of a graphical User Interface (Frontend).
+
+- Additional database schema support.
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@
 # Main entry point for the NL2SQL application
 import os
 from dotenv import load_dotenv
+from src.nl2sql.hf_engine import get_models, DEFAULT_MODEL_ID
 from src.scripts.interactive_mode import run_interactiveMode
 from src.scripts.evaluation_mode import run_evaluation
 
@@ -11,23 +12,64 @@ hf_token = os.getenv("HF_TOKEN")
 if not hf_token:
     raise ValueError("HuggingFace API token not found!")
 
+def select_model() -> str:
+    """
+    Allow user to choose NL2SQL models available.
+    """
+    available_models = get_models()
+    print("\n" + "="*30)
+    print(" Select Model for Testing:")
+    print("\n***Press q to quit at any time.")
+    print("\n" + "="*30)
+
+    for i, model in enumerate(available_models, 1):
+        print(f"{i}. {model}")
+    print(f"{len(available_models) + 1}. Use Default [Recommended]: ({DEFAULT_MODEL_ID})")
+    print("\n" + "-"*50)
+
+    while True:
+        choice = input(f"Select a model (1-{len(available_models) + 1}) or 'q' to quit: ")
+
+        if choice.lower() == 'q':
+            print("Returning to main menu.")
+            return None
+        
+        try:
+            choice = int(choice)
+
+            if 1 <= choice <= len(available_models):
+                selected = available_models[choice-1]
+                print(f"\n[+] Active Model set to: {selected}")
+                return selected
+            elif choice == len(available_models) + 1:
+                print(f"\n[+] Active Model set to Default: {DEFAULT_MODEL_ID}")
+                return DEFAULT_MODEL_ID
+            else:
+                print("Invalid range. Please select a valid option.")
+        except ValueError:
+            print("Invalid input. Please enter a number corresponding to the model choice.")
+
 def main():
     """Main application entry point and interactive menu"""
     while True:
         print("\n" + "="*30)
         print(" NL2SQL Application Main Menu")
         print("\n" + "="*30)
-        print("1. Run Interactive Agent NL2SQL Mode (Ask a single question)")
-        print("2. Run Batch Evaluation of NL2SQL Agent (Evaluate on 15 test cases)")
+        print("1. Run Interactive Agent NL2SQL Mode (Question Answering Evaluation)")
+        print("2. Run Batch Evaluation of NL2SQL Agent (Question to SQL Evaluation)")
         print("3. Exit")
         print("\n" + "="*30)
         
         choice = input("Select an option (1-3): ")
 
         if choice == '1':
-            run_interactiveMode()
+            selected_model = select_model()
+            if selected_model:
+                run_interactiveMode(model_id=selected_model)
         elif choice == '2':
-            run_evaluation()
+            selected_model = select_model()
+            if selected_model:
+                run_evaluation(model_id=selected_model)
         elif choice == '3':
             print("Exiting application. Goodbye!")
             break
